740652d080493142ea2725f38106ce85f7234654
Yago Matos <yago.matos@gentrop.com>
2020-07-30 16:46:25 -0300

    test: ensure Login present valid state if email is valid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -66,4 +66,14 @@ describe('Login Component', () => {
     expect(passwordStatus.title).toBe('Tudo Certo!')
     expect(passwordStatus.textContent).toBe('🟢')
   })
+
+  test('Should show valid email state if Validation successeds', () => {
+    const { sut, validationStub } = makeSut()
+    validationStub.errorMessage = null
+    const emailInput = sut.getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: faker.internet.email() } })
+    const emailStatus = sut.getByTestId('email-status')
+    expect(emailStatus.title).toBe('Tudo Certo!')
+    expect(emailStatus.textContent).toBe('🟢')
+  })
 })
