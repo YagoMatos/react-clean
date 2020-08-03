758048df508533de4a10efe52e2fd6de7d50945f
Yago Matos <yago.matos@gentrop.com>
2020-08-03 15:04:50 -0300

    test: ensure Login disable submit button only form is invalid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -76,4 +76,15 @@ describe('Login Component', () => {
     expect(emailStatus.title).toBe('Tudo Certo!')
     expect(emailStatus.textContent).toBe('🟢')
   })
+
+  test('Should enable submit button if form is valid', () => {
+    const { sut, validationStub } = makeSut()
+    validationStub.errorMessage = null
+    const emailInput = sut.getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: faker.internet.email() } })
+    const passwordInput = sut.getByTestId('password')
+    fireEvent.input(passwordInput, { target: { value: faker.internet.password() } })
+    const submitButtom = sut.getByTestId('submit') as HTMLButtonElement
+    expect(submitButtom.disabled).toBe(false)
+  })
 })
