195d086d849d78b15443e7e9a35a90c44fc81eae
Yago Matos <yago.matos@gentrop.com>
2021-03-28 15:15:07 -0300

    feat: ensure Signup change password and confirmation status to error if validation fails

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -13,6 +13,8 @@ const Signup: React.FC<Props> = ({ validation }: Props) => {
     isLoading: false,
     name: '',
     email: '',
+    password: '',
+    passwordConfirmation: '',
     nameError: '',
     emailError: '',
     passwordError: 'Campo Obrigatório',
@@ -24,9 +26,11 @@ const Signup: React.FC<Props> = ({ validation }: Props) => {
     setState({
       ...state,
       nameError: validation.validate('name', state.name),
-      emailError: validation.validate('name', state.name)
+      emailError: validation.validate('email', state.email),
+      passwordError: validation.validate('password', state.password),
+      passwordConfirmationError: validation.validate('passwordConfirmation', state.passwordConfirmation)
     })
-  }, [state.name, state.email])
+  }, [state.name, state.email, state.password, state.passwordConfirmation])
 
   return (
     <div className={Styles.signup}>
