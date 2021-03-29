2111a083d3c7fc50a21b286cceb218bc1a36ab4e
Yago Matos <yago.matos@gentrop.com>
2021-03-28 23:11:35 -0300

    refactor: change validation to receive an object instead of value

--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -2,19 +2,29 @@ import { CompareFieldsValidation } from './compare-fields-validation'
 import { InvalidFieldError } from '@/validation/errors'
 import faker from 'faker'
 
-const makeSut = (valueToCompare: string): CompareFieldsValidation => new CompareFieldsValidation(faker.database.column(), valueToCompare)
+const makeSut = (field: string, fieldToCompare: string): CompareFieldsValidation => new CompareFieldsValidation(field, fieldToCompare)
 
 describe('CompareFieldsValidation', () => {
   test('Should return error if field is compare is invalid', () => {
-    const sut = makeSut(faker.random.word())
-    const error = sut.validate(faker.random.word())
+    const field = faker.database.column()
+    const fieldToCompare = faker.database.column()
+    const sut = makeSut(field, fieldToCompare)
+    const error = sut.validate({
+      [field]: faker.database.column(),
+      [fieldToCompare]: faker.database.column()
+    })
     expect(error).toEqual(new InvalidFieldError())
   })
 
   test('Should return falsy if field is compare is valid', () => {
-    const valueToCompare = faker.random.word()
-    const sut = makeSut(valueToCompare)
-    const error = sut.validate(valueToCompare)
+    const field = faker.database.column()
+    const fieldToCompare = faker.random.word()
+    const value = faker.database.column()
+    const sut = makeSut(field, fieldToCompare)
+    const error = sut.validate({
+      [field]: value,
+      [fieldToCompare]: value
+    })
     expect(error).toBeFalsy()
   })
 })
--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -26,14 +26,14 @@ describe('ValidationComposite', () => {
     const errorMessage = faker.random.words()
     fieldValidationSpy[0].error = new Error(errorMessage)
     fieldValidationSpy[1].error = new Error(faker.random.words())
-    const error = sut.validate(fieldName, faker.random.word())
+    const error = sut.validate(fieldName, { [fieldName]: faker.random.word() })
     expect(error).toBe(errorMessage)
   })
 
   test('Should return falsy if there is no error', () => {
     const fieldName = faker.database.collation()
     const { sut } = makeSut(fieldName)
-    const error = sut.validate(fieldName, faker.random.word())
+    const error = sut.validate(fieldName, { [fieldName]: faker.random.word() })
     expect(error).toBeFalsy()
   })
 })
