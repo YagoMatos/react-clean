9e671fc8a54735f20938b94013a6404535b6a6c0
Yago Matos <yago.matos@gentrop.com>
2021-04-07 18:43:00 -0300

    feat: ensure PrivateRoute renders component if token is not empty

--- src/main/factories/pages/signup/signup-factories.tsx
+++ src/main/factories/pages/signup/signup-factories.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Signup } from '@/presentation/pages'
 import { makeSignupValidation } from './signup-validation-factory'
-import { makeLocalUpdateCurrentAccount } from '@/main/factories/usecases/save-access-token/local-save-access-token-factory'
 import { makeRemoteAddAccount } from '../../usecases/add-account/remote-add-account'
 
 export const makeSignup: React.FC = () => {
@@ -9,7 +8,6 @@ export const makeSignup: React.FC = () => {
     <Signup
       addAccount={makeRemoteAddAccount()}
       validation={makeSignupValidation()}
-      updateCurrentAccount={makeLocalUpdateCurrentAccount()}
     />
   )
 }
